12ed50f5d1c209225c9801edf00d53755e47af31
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2020-10-25 22:03:18 +0100

    Remove superfluous method

--- src/test/scala/ApiSpec.scala
+++ src/test/scala/ApiSpec.scala
@@ -14,23 +14,20 @@ import org.http4s.Charset
 import io.circe.Json
 
 class ApiSpec extends ApiSuite {
-  test("GET /hello should greet with the name that was provided via the corresponding query parameter") {
-    val response = api().run(Request[IO](method = GET, uri = uri"/hello?name=John%20Doe"))
-    check(response, Ok, "Hello John Doe!")
-  }
-
   test("GET /hello should greet the world if name parameter is omitted") {
     val response = api().run(Request[IO](method = GET, uri = uri"/hello"))
     check(response, Ok, "Hello World!")
+  }
+
+  test("GET /hello should greet with the name that was provided via the corresponding query parameter") {
+    val response = api().run(Request[IO](method = GET, uri = uri"/hello?name=John%20Doe"))
+    check(response, Ok, "Hello John Doe!")
   }
 }
 
 trait ApiSuite extends CatsEffectSuite {
   def api(serverUrl: String = "http://localhost:8080"): Kleisli[IO, Request[IO], Response[IO]] =
     Api[IO](ApiDocsConfiguration(serverUrl)).orNotFound
-
-  def check(responseIO: IO[Response[IO]], expectedStatus: Status): IO[Unit] =
-    check(responseIO, expectedStatus, None, None)
 
   def check(responseIO: IO[Response[IO]], expectedStatus: Status, expectedBody: Json): IO[Unit] =
     check(responseIO, expectedStatus, Some(expectedBody.noSpaces), Some(`Content-Type`(application.json)))
@@ -41,8 +38,8 @@ trait ApiSuite extends CatsEffectSuite {
   def check(
     io: IO[Response[IO]],
     expectedStatus: Status,
-    expectedBody: Option[String],
-    expectedContentType: Option[`Content-Type`]
+    expectedBody: Option[String] = None,
+    expectedContentType: Option[`Content-Type`] = None
   ): IO[Unit] = io.flatMap { response =>
     assertEquals(response.status, expectedStatus)
     assertEquals(response.headers.get(`Content-Type`), expectedContentType)
