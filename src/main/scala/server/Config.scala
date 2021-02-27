f7e795f71905bcbe6b5408c91057921aa05cb3d5
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-02-27 15:57:10 +0100

    Accumulate config errors

--- src/main/scala/server/Config.scala
+++ src/main/scala/server/Config.scala
@@ -9,13 +9,14 @@ case class ApiDocsConfig(serverUri: Uri, description: Option[String])
 
 package object app {
 
-  val config: ConfigValue[Config] =
-    for {
-      port            <- env("PORT").as[Int].default(8080)
-      docsServerUri   <- env("APIDOCS_SERVER_URI").as[Uri].default(Uri.unsafeFromString("http://localhost:8080"))
-      docsDescription <- env("APIDOCS_DESCRIPTION").option
-    } yield (Config(port, ApiDocsConfig(docsServerUri, docsDescription)))
-
   implicit private val uriConfigDecoder: ConfigDecoder[String, Uri] =
     ConfigDecoder[String, String].mapEither { case (_, v) => Uri.fromString(v).leftMap(e => ConfigError(e.details)) }
+
+  val config: ConfigValue[Config]                                   =
+    (
+      env("PORT").as[Int].default(8080),
+      env("APIDOCS_SERVER_URI").as[Uri].default(Uri.unsafeFromString("http://localhost:8080")),
+      env("APIDOCS_DESCRIPTION").option
+    ).parMapN { case (port, uri, description) => Config(port, ApiDocsConfig(uri, description)) }
+
 }
